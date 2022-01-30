efc6bc2bef600b44689f4d7317ea18e27c171ce3
Trinidad Mamud <trinidad.mamud@mercadolibre.com>
2022-01-30 15:22:39 -0300

    set up de proyecto y redirect to Home component

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+
+const Home = () => {
+
+    return (
+        <>
+            <header className="App-header">
+        <p>
+          HOLA <code>SI</code> FUNCIONA.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+        </>
+    )
+}
+
+export default Home;
